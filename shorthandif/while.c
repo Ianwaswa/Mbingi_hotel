47aad542b7cf8adde770e129af0a0bb6befa1faf
Ianwaswa <ianwaswa2019@gmail.com>
2023-07-06 08:52:00 -0700

    task 1

---
+++ shorthandif/while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Loops can execute a block of code as long as a specified condition is reached
+ * The while loop loops through a block of code as long as a specified condition is true
+ * Loops are handy because they save time, reduce errors, and they make code more readable
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 5)
+	{
+	printf("%d\n", i);
+	i++;
+	}
+}
